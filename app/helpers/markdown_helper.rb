aaf23dfa0f4aa61a8b9e2978682d2c1c4c8bbbf5
NobuhideMine <141533543+NobuhideMine@users.noreply.github.com>
2023-11-16 03:49:24 +0000

    [Add]

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,14 +1,17 @@
+
+
+require 'redcarpet'
+#require 'rouge'
 require 'rouge/plugins/redcarpet'
 
 class CustomRenderHTML < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
 end
 
-
 module MarkdownHelper
   def markdown(text)
     options = {
-      no_styles:     true,
+      #no_styles:     true,
       with_toc_data: true,
       hard_wrap:     true
     }
@@ -18,18 +21,25 @@ module MarkdownHelper
       fenced_code_blocks:  true,
       autolink:            true,
       lax_spacing:         true,
-      space_after_headers: true
+      space_after_headers: true,
+      highlight:           true,
+      lax_html_blocks:     true,
+      footnotes:           true,
+      strikethrough:       true,
+      underline:           true,
+      quote:               true
     }
     
-
-    renderer = CustomRenderHTML.new(options)
+   
+  
+   renderer = CustomRenderHTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
-    markdown.render(text).html_safe
+    markdown.render(text)
   end
 
   def toc(text)
     renderer = Redcarpet::Render::HTML_TOC.new(nesting_level: 6)
     markdown = Redcarpet::Markdown.new(renderer)
-    markdown.render(text).html_safe
+    markdown.render(text)
   end
 end
