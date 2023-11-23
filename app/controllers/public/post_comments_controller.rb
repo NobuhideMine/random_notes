1c5c2faabb515410412f4588b6dbba7e05c19198
NobuhideMine <141533543+NobuhideMine@users.noreply.github.com>
2023-11-23 10:14:18 +0000

    [Update]

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -8,13 +8,11 @@ class Public::PostCommentsController < ApplicationController
         @comment = current_user.post_comments.new(post_comment_params)
         @comment.post_id = post.id
         @comment.save
-        #redirect_to request.referer
     end
 
     def destroy
         @comment = PostComment.find(params[:id])
         @comment.destroy
-    #redirect_to request,referer
     end
 
     private
